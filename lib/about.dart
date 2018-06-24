28d0f5c5829d5706b9afe5c437fda643efd231b0
Lastmentor <riza.gs1905@hotmail.com>
2018-06-25 01:09:38 +0300

    About Screen UI Updated

--- lib/about.dart
+++ lib/about.dart
@@ -1,48 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUs extends StatelessWidget {
   static const String routeName = "/AboutUs";
   @override
   Widget build(BuildContext context) {
 
-    final alucard = Hero(
-      tag: 'hero',
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: CircleAvatar(
-          radius: 70.0,
-          backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('images/logo.png'),
-        ),
-      ),
+    final alucard =  new Image(
+      image: new AssetImage("images/logo.png"),width: 150.0,height: 150.0,
     );
 
     final welcome = Padding(
       padding: EdgeInsets.all(8.0),
       child: Text(
         'Welcome Flutter Travel',
-        style: TextStyle(fontSize: 28.0, color: Colors.white),
-      ),
-    );
-
-    final back = new Padding(
-      padding: const EdgeInsets.only(top: 4.0),
-      child: new Row(
-        children: <Widget>[
-          new IconButton(icon:
-          new Icon(Icons.arrow_back_ios,color: Colors.white,),
-              onPressed: () {
-                Navigator.of(context).pop();
-              }),
-        ],
+        style: TextStyle(fontSize: 28.0, color: const Color(0xFF003333)),
       ),
     );
 
     final lorem = Padding(
       padding: EdgeInsets.all(8.0),
       child: Text(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit condimentum mauris id tempor. Praesent eu commodo lacus. Praesent eget mi sed libero eleifend tempor. Sed at fringilla ipsum. Duis malesuada feugiat urna vitae convallis. Aliquam eu libero arcu.',
-        style: TextStyle(fontSize: 16.0, color: Colors.white,),textAlign: TextAlign.center,
+        'Flutter Travel is an application which you can share photos and videos around places where you travel in the world and chat with your friends about what they shared',
+        style: TextStyle(fontSize: 16.0, color: const Color(0xFF003333),),textAlign: TextAlign.center,
+      ),
+    );
+
+    final button = new Opacity(
+      opacity: 0.7,
+      child: new Container(
+        width: 280.0,
+        height: 55.0,
+        child: new RaisedButton(
+          onPressed: _launchURL,
+          child: new Text("Visit Facebook Page",
+              style: new TextStyle(
+                  color: Colors.white, fontSize: 20.0)),
+          color: const Color(0xFF006633),
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(30.0)),
+        ),
       ),
     );
 
@@ -51,17 +48,52 @@ class AboutUs extends StatelessWidget {
       padding: EdgeInsets.all(28.0),
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [
-          Colors.blue,
-          Colors.lightBlueAccent,
+          const Color(0xFFD6A4A4),
+          const Color(0xFFFFEDBC),
         ]),
       ),
-      child: Column(
-        children: <Widget>[back, alucard, welcome, lorem],
+      child: new ListView(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              new IconButton(
+                  icon: new Icon(
+                    Icons.arrow_back_ios,
+                    size: 35.0,
+                    color: const Color(0xFF003333),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          ),
+          new Center(
+            child: new Column(
+              children: <Widget>[
+                new Padding(padding: const EdgeInsets.only(top: 15.0)),
+                alucard,
+                welcome,
+                lorem,
+                new Padding(padding: const EdgeInsets.only(top: 20.0)),
+                button
+              ],
+            ),
+          )
+        ],
       ),
     );
 
-    return Scaffold(
+    return new Scaffold(
       body: body,
     );
+  }
+
+  void _launchURL() async {
+    const url = 'https://www.facebook.com/Flutter-Travel-198549500800595/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
